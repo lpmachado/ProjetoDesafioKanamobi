00992a1b4f60743656b2947472ef07343417fcc2
Luis Paulo <luispaulomac@gmail.com>
2018-08-05 17:21:39 -0300

    Projeto que lista repositorios populares de Java no GitHub

---
+++ app/src/androidTest/java/com/example/luispaulo/kanamobi/ExampleInstrumentedTest.kt
@@ -0,0 +1,30 @@
+/**package com.example.luispaulo.kanamobi;
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // TODO Escrever alguns casos de testes para os casos de uso
+
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.", appContext.getPackageName())
+    }
+}
+/**
